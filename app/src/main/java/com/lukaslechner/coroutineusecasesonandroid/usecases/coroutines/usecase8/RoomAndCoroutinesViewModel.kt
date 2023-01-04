8195d5d98c02c74efc18874e8d92ed507a35e38a
gitlood <unilood@gmail.com>
2023-01-05 10:31:52 +1300

    Room with coroutines

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
@@ -1,7 +1,10 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase8
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RoomAndCoroutinesViewModel(
     private val api: MockApi,
@@ -9,15 +12,39 @@ class RoomAndCoroutinesViewModel(
 ) : BaseViewModel<UiState>() {
 
     fun loadData() {
+        uiState.value = UiState.Loading.LoadFromDb
 
+        viewModelScope.launch {
+            val localVersions = database.getAndroidVersions()
+            if (localVersions.isEmpty()) {
+                uiState.value = UiState.Error(DataSource.DATABASE, "Database is empty")
+            } else {
+                uiState.value =
+                    UiState.Success(DataSource.DATABASE, localVersions.mapToUiModelList())
+            }
+
+            uiState.value = UiState.Loading.LoadFromNetwork
+            try {
+                val recentVersions = api.getRecentAndroidVersions()
+                recentVersions.forEach { androidVersion ->
+                    database.insert(androidVersion.mapToEntity())
+                }
+                uiState.value = UiState.Success(DataSource.NETWORK, recentVersions)
+            } catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error(DataSource.NETWORK, "Something went wrong")
+            }
+        }
     }
 
     fun clearDatabase() {
-
+        viewModelScope.launch {
+            database.clear()
+        }
     }
 }
 
-enum class DataSource(val dataSourceName: String) {
-    DATABASE("Database"),
-    NETWORK("Network")
+enum class DataSource {
+    DATABASE,
+    NETWORK
 }
