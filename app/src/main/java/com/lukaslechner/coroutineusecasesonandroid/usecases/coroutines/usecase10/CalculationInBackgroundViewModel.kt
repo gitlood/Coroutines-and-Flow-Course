0c31447c9f14efc033c04914bb51feae7dba34cb
gitlood <unilood@gmail.com>
2023-01-05 12:06:05 +1300

    Using withContext to switch from Main Thread to Dispatcher.IO context

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
@@ -2,7 +2,10 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import timber.log.Timber
 import java.math.BigInteger
 import kotlin.system.measureTimeMillis
 
@@ -14,22 +17,26 @@ class CalculationInBackgroundViewModel : BaseViewModel<UiState>() {
         var resultString: String
 
         viewModelScope.launch {
-            val computationDuration = measureTimeMillis {
+
+            Timber.d("Coroutine Context: $coroutineContext")
+
+            val computationDuration: Long = measureTimeMillis {
                 result = calculateFactorialOf(factorialOf)
             }
-            val stringConversionDuration = measureTimeMillis {
-                resultString = result.toString()
+            val stringConversionDuration: Long = measureTimeMillis {
+                resultString = withContext(Dispatchers.IO) { result.toString() }
             }
+
             uiState.value =
                 UiState.Success(resultString, computationDuration, stringConversionDuration)
         }
     }
 
-    private fun calculateFactorialOf(number: Int): BigInteger {
+    private suspend fun calculateFactorialOf(number: Int) = withContext(Dispatchers.IO) {
         var factorial = BigInteger.ONE
         for (i in 1..number) {
             factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
         }
-        return factorial
+        factorial
     }
 }
