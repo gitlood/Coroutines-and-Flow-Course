f62b581d0c02e0c91ae6c778dafbd07db21fc0af
gitlood <unilood@gmail.com>
2023-01-05 10:07:12 +1300

    Combining timeout and retry

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -3,6 +3,7 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -18,20 +19,35 @@ class RetryNetworkRequestViewModel(
             val numberOfTries = 2
 
             try {
-                repeat(numberOfTries) {
-                    try {
-                        loadRecentAndroidVersions()
-                        return@launch
-                    } catch (exception: Exception) {
-                        Timber.e(exception)
-                    }
+                retry(numberOfTries) {
+                    loadRecentAndroidVersions()
                 }
-                loadRecentAndroidVersions()
             } catch (e: Exception) {
                 Timber.e(e)
                 uiState.value = UiState.Error("Network request failed.")
             }
         }
+    }
+
+    private suspend fun <T> retry(
+        numberOfRetries: Int,
+        initialDelayMillis: Long = 100,
+        maxDelayMillis: Long = 1000,
+        factor: Double = 2.0,
+        block: suspend () -> T
+    ): T {
+        var currentDelay = initialDelayMillis
+        repeat(numberOfRetries) {
+            try {
+                return block()
+            } catch (exception: Exception) {
+                Timber.e(exception)
+            }
+            delay(currentDelay)
+
+            currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelayMillis)
+        }
+        return block()
     }
 
     private suspend fun loadRecentAndroidVersions() {
