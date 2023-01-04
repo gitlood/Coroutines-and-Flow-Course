a9378a614f6510ad204b3010372f0118059f24c8
gitlood <unilood@gmail.com>
2023-01-05 09:23:12 +1300

    Retry

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,42 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+
+            val numberOfTries = 2
+
+            try {
+                repeat(numberOfTries) {
+                    try {
+                        loadRecentAndroidVersions()
+                        return@launch
+                    } catch (exception: Exception) {
+                        Timber.e(exception)
+                    }
+                }
+                loadRecentAndroidVersions()
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error("Network request failed.")
+            }
+        }
+    }
+
+    private suspend fun loadRecentAndroidVersions() {
+        val recentAndroidVersions = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(recentAndroidVersions)
     }
 
 }
