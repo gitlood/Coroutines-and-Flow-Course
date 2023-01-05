90e8664a144ca9a9c6f4d14caa619be6e2299c26
gitlood <unilood@gmail.com>
2023-01-05 15:09:19 +1300

    Parents wait for children

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/structuredconcurrency/3_parents_wait_for_children.kt
@@ -0,0 +1,23 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.structuredconcurrency
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+
+    val scope = CoroutineScope(Dispatchers.Default)
+
+    val parentCoroutineJob = scope.launch {
+        launch {
+            delay(1000)
+            println("Child Coroutine 1 has completed")
+        }
+        launch {
+            delay(1000)
+            println("Child Coroutine 2 has completed")
+        }
+    }
+
+    parentCoroutineJob.join()
+
+    println("Parent coroutine has completed!")
+}
