8cbb487e84aa53ac08416f6b80238e2a50b90d92
gitlood <unilood@gmail.com>
2023-01-05 19:47:14 +1300

    Cooperative cancellation

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/cancellation/2_cooperative_cancellation.kt
@@ -0,0 +1,26 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.cancellation
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    val job = launch(Dispatchers.Default) {
+        repeat(10) { index ->
+            //ensureActive()
+            //yield()
+            if (isActive) {
+                println("operation number $index")
+                Thread.sleep(100)
+            } else {
+                //clean up operations
+                println("Cleaning up")
+                throw CancellationException()
+           //     return@launch
+            }
+        }
+
+    }
+
+    delay(250)
+    println("Cancelling Coroutine")
+    job.cancel()
+}
