1f27a2a218d16350755873fc94ad53ac3b5d2563
gitlood <unilood@gmail.com>
2023-01-06 09:48:50 +1300

    Implementing exception handler

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
@@ -1,20 +1,44 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase13
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
-import kotlinx.coroutines.*
-import timber.log.Timber
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class ExceptionHandlingViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun handleExceptionWithTryCatch() {
-
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            //try catch is only necessary if you want to recover from the exception
+            try {
+                api.getAndroidVersionFeatures(27)
+            } catch (e: Exception) {
+                if (e is HttpException) {
+                    if (e.code() == 500) {
+                        //error message
+                    } else {
+                        //error message
+                    }
+                }
+                uiState.value = UiState.Error("Network request failed: $e")
+            }
+        }
     }
 
     fun handleWithCoroutineExceptionHandler() {
+        uiState.value = UiState.Loading
 
+        val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            uiState.value = UiState.Error("Network Request Failed!")
+        }
+        viewModelScope.launch(exceptionHandler) {
+            api.getAndroidVersionFeatures(27)
+        }
     }
 
     fun showResultsEvenIfChildCoroutineFails() {
