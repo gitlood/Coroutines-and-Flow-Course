3efb613dd1504daa46c5bd29364154958fa92408
gitlood <unilood@gmail.com>
2023-01-05 18:29:40 +1300

    Scoping functions

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/structuredconcurrency/5_globalscope.kt
@@ -0,0 +1,14 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.structuredconcurrency
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+fun main(){
+
+    println("Job of GlobalScope: ${GlobalScope.coroutineContext[Job]}")
+
+    GlobalScope.launch {
+
+    }
+}
--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModel.kt
@@ -3,6 +3,7 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -13,13 +14,22 @@ class PerformSingleNetworkRequestViewModel(
     fun performSingleNetworkRequest() {
         uiState.value = UiState.Loading
 
-        viewModelScope.launch {
+        val job = viewModelScope.launch {
+            Timber.d("I am the first statement in the coroutine")
             try {
                 val recentAndroidVersions = mockApi.getRecentAndroidVersions()
                 uiState.value = UiState.Success(recentAndroidVersions)
             } catch (e: Exception) {
                 Timber.e(e)
                 uiState.value = UiState.Error("Network request failed.")
+            }
+        }
+
+        println("I am the first statement after launching the coroutine.")
+
+        job.invokeOnCompletion { throwable ->
+            if (throwable is CancellationException) {
+                Timber.d("Coroutine was cancelled")
             }
         }
     }
