84bf249976f5558bda15a95a9b50bc9624253411
gitlood <unilood@gmail.com>
2023-01-07 09:19:25 +1300

    Flow pipeline onState onEach map(transform emission type)
    and launchIn instead of collect for readability

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -3,7 +3,10 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase1
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 
 class FlowUseCase1ViewModel(
     stockPriceDataSource: StockPriceDataSource
@@ -12,10 +15,35 @@ class FlowUseCase1ViewModel(
     val currentStockPriceAsLiveData: MutableLiveData<UiState> = MutableLiveData()
 
     init {
-        viewModelScope.launch {
-            stockPriceDataSource.latestStockList.collect { stockList ->
-                currentStockPriceAsLiveData.value = UiState.Success(stockList)
+//        viewModelScope.launch {
+//            stockPriceDataSource.latestStockList.collect { stockList ->
+//                currentStockPriceAsLiveData.value = UiState.Success(stockList)
+//            }
+//        }
+
+        //less nesting using launchin
+        stockPriceDataSource
+            .latestStockList
+            .map { listOfStocks ->
+                UiState.Success(listOfStocks) as UiState
             }
-        }
+            .onStart {
+                emit(UiState.Loading)
+                //  Timber.d("Flow starts to be collected 1")
+            }
+            //above this is the upstream completion block
+            //below completion block is downstream
+//            .onCompletion { cause ->
+//                Timber.d("Flow starts has completed!.")
+//                //emit affect downstream
+//                emit(emptyList())
+//            }
+            .onEach { UiState ->
+                currentStockPriceAsLiveData.value = UiState
+            }
+//            .onStart {
+//                Timber.d("Flow starts to be collected 2")
+//            }
+            .launchIn(viewModelScope)
     }
 }
