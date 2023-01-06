3448f60bb3f4ede02911dfcd8f94a50557b84d4c
gitlood <unilood@gmail.com>
2023-01-07 09:39:42 +1300

    As Live data Timeouts, Dispatchers = big tidy up

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -1,49 +1,27 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase1
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
-import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
+import timber.log.Timber
 
 class FlowUseCase1ViewModel(
     stockPriceDataSource: StockPriceDataSource
 ) : BaseViewModel<UiState>() {
 
-    val currentStockPriceAsLiveData: MutableLiveData<UiState> = MutableLiveData()
-
-    init {
-//        viewModelScope.launch {
-//            stockPriceDataSource.latestStockList.collect { stockList ->
-//                currentStockPriceAsLiveData.value = UiState.Success(stockList)
-//            }
-//        }
-
-        //less nesting using launchin
-        stockPriceDataSource
-            .latestStockList
-            .map { listOfStocks ->
-                UiState.Success(listOfStocks) as UiState
-            }
-            .onStart {
-                emit(UiState.Loading)
-                //  Timber.d("Flow starts to be collected 1")
-            }
-            //above this is the upstream completion block
-            //below completion block is downstream
-//            .onCompletion { cause ->
-//                Timber.d("Flow starts has completed!.")
-//                //emit affect downstream
-//                emit(emptyList())
-//            }
-            .onEach { UiState ->
-                currentStockPriceAsLiveData.value = UiState
-            }
-//            .onStart {
-//                Timber.d("Flow starts to be collected 2")
-//            }
-            .launchIn(viewModelScope)
-    }
+    val currentStockPriceAsLiveData: LiveData<UiState> = stockPriceDataSource
+        .latestStockList
+        .map { listOfStocks ->
+            UiState.Success(listOfStocks) as UiState
+        }
+        .onStart {
+            emit(UiState.Loading)
+        }
+        .onCompletion {
+            Timber.tag("Flow").d("Flow has completed.")
+        }
+        .asLiveData()
 }
