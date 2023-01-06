69efa6fc632a9d3970cc8f5f1f9f1e8b82d14d91
gitlood <unilood@gmail.com>
2023-01-06 15:03:45 +1300

    Controlling execution, testing if scope is cancelled

---
+++ app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase14/AndroidVersionRepositoryTest.kt
@@ -0,0 +1,75 @@
+package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase14
+
+import com.lukaslechner.coroutineusecasesonandroid.mock.mockAndroidVersions
+import com.lukaslechner.coroutineusecasesonandroid.utils.MainDispatcherRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class AndroidVersionRepositoryTest() {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @Test
+    fun `getLocalAndroidVersions() should return android versions from database`() = runTest {
+        val fakeDatabase = FakeDatabase()
+
+        val repository = AndroidVersionRepository(fakeDatabase, this)
+        assertEquals(mockAndroidVersions, repository.getLocalAndroidVersions())
+    }
+
+    @Test
+    fun `loadRecentAndroidVersions() should return android versions from network`() = runTest {
+        val fakeDatabase = FakeDatabase()
+        val fakeApi = FakeApi()
+        val repository = AndroidVersionRepository(
+            fakeDatabase,
+            this,
+            api = fakeApi
+        )
+        assertEquals(mockAndroidVersions, repository.loadAndStoreRemoteAndroidVersions())
+    }
+
+    @Test
+    fun `loadRecentAndroidVersions() should continue to load and store android versions when calling scope gets cancelled`() =
+        runTest {
+            val fakeDatabase = FakeDatabase()
+            val fakeApi = FakeApi()
+            val applicationScope = this
+            val repository = AndroidVersionRepository(
+                fakeDatabase,
+                applicationScope,
+                api = fakeApi
+            )
+
+            // Sharing the testScheduler with the applicationScope is important!
+            val viewModelScope = TestScope(this.testScheduler)
+            val job = viewModelScope.launch {
+                repository.loadAndStoreRemoteAndroidVersions()
+            }
+
+            // execute coroutine until delay(1) in the fakeApi
+            applicationScope.runCurrent()
+
+            // Check if nothing is inserted into the db before we cancel the scope
+            assertEquals(false, fakeDatabase.insertedIntoDb)
+
+            // Cancel the scope and check if it was indeed cancelled
+            viewModelScope.cancel()
+            assertEquals(true, job.isCancelled)
+
+            // continue coroutine execution after delay(1) in the fakeApi
+            applicationScope.advanceTimeBy(1)
+            applicationScope.runCurrent()
+
+            assertEquals(true, fakeDatabase.insertedIntoDb)
+        }
+}
