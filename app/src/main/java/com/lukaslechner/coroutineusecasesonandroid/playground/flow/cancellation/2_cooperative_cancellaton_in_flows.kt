612a1d0f8cb5092f4cf651214a18dfe972f7e89e
gitlood <unilood@gmail.com>
2023-01-08 12:53:08 +1300

    Cancellation operator

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/cancellation/2_cooperative_cancellaton_in_flows.kt
@@ -0,0 +1,50 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.flow.cancellation
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onCompletion
+import java.math.BigInteger
+import kotlin.coroutines.EmptyCoroutineContext
+
+suspend fun main() {
+    val scope = CoroutineScope(EmptyCoroutineContext)
+
+    scope.launch {
+        intFlow()
+            .onCompletion { throwable ->
+                if (throwable is CancellationException) {
+                    println("Flow got cancelled")
+                }
+            }
+            .collect {
+                println("Collected $it")
+
+                if (it == 2) {
+                    cancel()
+                }
+            }
+    }.join()
+}
+
+private fun intFlow() = flow {
+    emit(1)
+    emit(2)
+
+    //currentCoroutineContext().ensureActive()
+
+    println("Start calculation")
+    calculateFactorialOf(1000)
+    println("Calculation finished")
+
+    //emit checks whether flow is cancelled internally
+    emit(3)
+}
+
+private suspend fun calculateFactorialOf(number: Int): BigInteger = coroutineScope {
+    var factorial = BigInteger.ONE
+    for (i in 1..number) {
+        factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
+        ensureActive()
+    }
+    factorial
+}
