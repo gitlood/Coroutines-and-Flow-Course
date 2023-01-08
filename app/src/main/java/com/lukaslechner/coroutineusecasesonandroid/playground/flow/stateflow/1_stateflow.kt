f43d95606ad5e1ea39f35350b09e7f0a42911e8d
gitlood <unilood@gmail.com>
2023-01-09 06:16:08 +1300

    State flows An optimised version of shared flows(replay = 1)

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/stateflow/1_stateflow.kt
@@ -0,0 +1,28 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.flow.stateflow
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+suspend fun main() {
+
+    val counter = MutableStateFlow(0)
+
+    println(counter.value)
+
+    counter.value = 3
+
+    coroutineScope {
+        repeat(10_000) {
+            launch {
+                counter.update { currentValue ->
+                    currentValue + 1
+                }
+            }
+        }
+    }
+
+    println(counter.value)
+
+}
